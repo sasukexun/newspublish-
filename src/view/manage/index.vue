65225fefd5971370717912576a48a194fddeaa38
sasukexun <736301835@qq.com>
2018-08-14 16:25:16 +0800

    新增删除功能

--- src/view/manage/index.vue
+++ src/view/manage/index.vue
@@ -10,15 +10,19 @@
         <th>姓名</th>
         <th>操作</th>
       </tr>
-      <tr v-for="item in peoples">
+      <tr v-for="(item,index) in peoples">
         <td>{{item.name}}</td>
-        <td><span>编辑</span><span>删除</span></td>
+        <td v-bind:id="index">
+          <span>编辑</span>
+          <span v-on:click="del">删除</span>
+        </td>
       </tr>
     </table>
     <FooterNav v-bind:class="{'isManage':isNowPage}"></FooterNav>
   </div>
 </template>
 <script>
+  //v-model在表单控件或者组件上创建双向绑定
   import FooterNav from '../../components/footer'
   export default{
     components: {
@@ -37,15 +41,21 @@
         this.showAdd=true
       },
       addName(){
-        var v = this.nameValue;
+        var v=this.nameValue;
         //trim - 输入首尾空格过滤
-        if(v.trim() == ""){
-          console.log("请输入新增人员姓名")
+        if(v.trim()==""){
+          console.log(请输入姓名)
         }else{
-          var data = {};
-          data.name = v;
+          var data={};
+          data.name=v;
+          //push() 方法可向数组的末尾添加一个或多个元素，并返回新的长度。
           this.peoples.push(data)
         }
+      },
+      del(e){
+        //offsetParent就是距离该子元素最近的进行过定位的父元素
+        var id=e.target.offsetParent.id;
+        this.peoples.splice(id,1)
       }
     }
   }
