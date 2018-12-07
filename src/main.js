0146ddc840c577cda38d1f66152fdd7fc603f1f3
sasukexun <736301835@qq.com>
2018-12-07 17:45:56 +0800

    修改文件，注册部件css样式

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import resource from './resource';//通过import引入
+import store from './vuex/index'
 import './assets/js/rem'
 //监听点击事件
 import FastClick from 'fastclick'
@@ -18,6 +19,7 @@ new Vue({
   el: '#app',
   router,
   resource,//通过import引入，然后在这里调用
+  store,
   components: { App },
   template: '<App/>'
 });
